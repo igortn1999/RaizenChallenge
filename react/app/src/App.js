622138352fdb45b97241c256b69cd51145cae46d
Jan Merkel <janpetermr@gmail.com>
2022-08-16 12:18:21 -0300

    Front end se comunicando com back end

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
+
+
+
 
 function App() {
+  const [painel, setPainel] = useState({id:"",name:""});
+
+  useEffect(() => {
+    const get_panel = async () => {
+      const res = await fetch("http://localhost:8080/api", {
+        method: "GET",
+      }).then(async(data)=>{
+        let body = await data.json()
+        setPainel(body);
+
+      })
+      
+    };
+    get_panel();
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Nome: {painel.name}</p>
+        <p>N Serie: {painel.id}</p>
           Learn React
-        </a>
       </header>
     </div>
   );
