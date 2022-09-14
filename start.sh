4c2aa0b85e91e723006126d9a4c9172e8155a2ef
Igor Tomazelli Nietmann <igortn@ibm.com>
2022-09-13 21:30:34 -0300

    Fixing start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ cd $WORK/react/app
 npm install
 
 cd  $WORK/docker-compose/postgresSQL/
-FILE=ls *.yml | grep .yml #TODO fix docker-compose command
+#FILE=ls *.yml | grep .yml #TODO fix docker-compose command
 docker-compose -f $(ls *.yml) up -d
