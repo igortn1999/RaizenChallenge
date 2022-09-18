c446b67736627a755ce1db62cf4fd6b37b6ecdc6
Igor Tomazelli Nietmann <igortn@ibm.com>
2022-09-17 21:23:47 -0300

    Solar quotas and Users relationship

--- src/main/java/com/app/raizen/services/ServiceDevice.java
+++ src/main/java/com/app/raizen/services/ServiceDevice.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.app.raizen.models.Address;
 import com.app.raizen.models.Device;
-import com.app.raizen.repositories.AddressRepository;
 import com.app.raizen.repositories.DeviceRepository;
 
 @Service
@@ -14,9 +12,6 @@ public class ServiceDevice {
 	
 	@Autowired
 	DeviceRepository dr;
-	
-	@Autowired
-	AddressRepository ar;
 	
 	@Transactional
 	public Object save(Device device) {
