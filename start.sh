835b0307147a62ed187d0bf030be5affad4f8f11
Igor Tomazelli Nietmann <igortn@ibm.com>
2022-09-19 20:38:30 -0300

    Added backup jar for envs without maven

--- start.sh
+++ start.sh
@@ -2,7 +2,20 @@
 
 WORK=$(PWD)
 cd $WORK/react/app
+
+#update node/dependencies
 npm install
-#docker network create db
+
+#make sure there is a jar in target folder. If there aren't any, copy from backup
+JAR=$(ls -1 target/ | grep -c .jar)
+
+
+if [ $JAR -eq "0" ] 
+then
+    cp $WORK/javaDeploy/*.jar $WORK/target
+    echo "no jar found, copying .jar from javaDeploy/"
+fi
+
+#start docker-compose
 cd  $WORK/docker-compose/postgresSQL/
 docker-compose -f $(ls *.yml) up -d
