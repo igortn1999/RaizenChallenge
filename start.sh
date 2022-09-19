b68299dd9c9f6386fc2a5204ee9303708c718aaa
Igor Tomazelli Nietmann <igortn@ibm.com>
2022-09-19 20:43:07 -0300

    Making start.sh more reliable

--- start.sh
+++ start.sh
@@ -9,10 +9,9 @@ npm install
 #make sure there is a jar in target folder. If there aren't any, copy from backup
 JAR=$(ls -1 target/ | grep -c .jar)
 
-
 if [ $JAR -eq "0" ] 
 then
-    cp $WORK/javaDeploy/*.jar $WORK/target
+    cp $WORK/javaDeploy/*.jar $WORK/target/raizen-api.jar
     echo "no jar found, copying .jar from javaDeploy/"
 fi
 
